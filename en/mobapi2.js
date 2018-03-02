a177616f5c165c8a61bf9b4b80bf8bb71386574b
matsueT5374 <mishima001@users.noreply.github.com>
2018-03-02 16:36:15 +0900

    Update mobapi2.js

--- en/mobapi2.js
+++ en/mobapi2.js
@@ -1,41 +1,45 @@
 (function getJSON() {
 
-    window.alert('①');
+    window.alert('①◇');
 
 let http = require('http');
-const URL = 'http://qiita.com/kazuhikoyamashita/items/273692ccbdf8c0950a71.json';
 
     window.alert('②');
 
 
-http.get(URL, (res) => {
+const URL = 'http://qiita.com/kazuhikoyamashita/items/273692ccbdf8c0950a71.json';
 
     window.alert('③');
 
-  let body = '';
+
+http.get(URL, (res) => {
 
     window.alert('④');
 
-  res.setEncoding('utf8');
+  let body = '';
 
     window.alert('⑤');
+
+  res.setEncoding('utf8');
+
+    window.alert('⑥');
 
   res.on('data', (chunk) => {
       body += chunk;
   });
 
-    window.alert('⑥');
+    window.alert('⑦');
 
   res.on('end', (res) => {
       res = JSON.parse(body);
 
-    window.alert('⑦' + res);
+    window.alert('⑧' + res);
 
       console.log(res);
   });
 }).on('error', (e) => {
 
-    window.alert('③' + e.message);
+    window.alert('⑨' + e.message);
 
   console.log(e.message); //エラー時
 });
