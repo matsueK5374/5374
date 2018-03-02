2cead06558155b0670f0e165a4266f6ce2938615
matsueT5374 <mishima001@users.noreply.github.com>
2018-03-02 15:06:31 +0900

    Update mobapi2.js

--- en/mobapi2.js
+++ en/mobapi2.js
@@ -1,15 +1,15 @@
-function getJSON() {
+(function getJSON() {
 
-    window.alert('①');
+    window.alert('①◆');
 
 
-    const request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
 
-    window.alert('②');
+    window.alert('②◆');
 
     //request.open("GET", `http://mob.tpj.co.jp/mob/api/records/41`);
     request.open("GET", "http://mob.tpj.co.jp/mob/api/records/41", true);
-    window.alert('③');
+    window.alert('③◆');
     //request.addEventListener("load", (event) => {
 
     request.onload = function (e) {
@@ -17,17 +17,7 @@ function getJSON() {
     window.alert('ステータス：' + request.statusText);
     window.alert('なかみ：' + request.responseText);
 
-    });
+    };
     request.send(null);
 
-
-
-  //var req = new XMLHttpRequest();		  // XMLHttpRequest オブジェクトを生成する
-  //req.onreadystatechange = function() {		  // XMLHttpRequest オブジェクトの状態が変化した際に呼び出されるイベントハンドラ
-  //  if(req.readyState == 4 && req.status == 200){ // サーバーからのレスポンスが完了し、かつ、通信が正常に終了した場合
-  //    alert(req.responseText);		          // 取得した JSON ファイルの中身を表示
-  //  }
-  //};
-  //req.open("GET", "http://mob.tpj.co.jp/mob/api/records/41", false); // HTTPメソッドとアクセスするサーバーの　URL　を指定
-  //req.send(null);					    // 実際にサーバーへリクエストを送信
-}
+})();
