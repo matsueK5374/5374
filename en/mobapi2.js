ff393d455e5bf55d66c64b79308b651003a75946
matsueT5374 <mishima001@users.noreply.github.com>
2018-03-02 16:23:33 +0900

    Update mobapi2.js

--- en/mobapi2.js
+++ en/mobapi2.js
@@ -5,18 +5,31 @@
 let http = require('http');
 const URL = 'http://qiita.com/kazuhikoyamashita/items/273692ccbdf8c0950a71.json';
 
+    window.alert('②');
+
+
 http.get(URL, (res) => {
+
+    window.alert('③');
+
   let body = '';
+
+    window.alert('④');
+
   res.setEncoding('utf8');
+
+    window.alert('⑤');
 
   res.on('data', (chunk) => {
       body += chunk;
   });
 
+    window.alert('⑥');
+
   res.on('end', (res) => {
       res = JSON.parse(body);
 
-    window.alert('②' + res);
+    window.alert('⑦' + res);
 
       console.log(res);
   });
